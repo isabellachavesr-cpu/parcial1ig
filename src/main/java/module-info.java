d28bfa402a44ac816867b47a6e96b0bd23874fa5
Isabella <isabella.chavesr@uqvirtual.edu.co>
2026-09-24 20:53:07 -0500

    inicializando el proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.parcial1ig {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.parcial1ig to javafx.fxml;
+    exports com.example.parcial1ig;
+}
