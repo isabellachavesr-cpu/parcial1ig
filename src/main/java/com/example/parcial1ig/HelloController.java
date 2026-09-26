c6ef39b17642bf10650757b751a9bb3b6cc4e5bc
Reinel Osorio <rosorior@uqvirtual.edu.co>
2026-09-26 00:26:36 -0500

    Prueba

--- src/main/java/com/example/parcial1ig/HelloController.java
+++ src/main/java/com/example/parcial1ig/HelloController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+// prueba
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
