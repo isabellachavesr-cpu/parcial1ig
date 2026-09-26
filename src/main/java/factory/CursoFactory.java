751ebb3ae7075b7abc352d481c2ea6a5fcfc0280
Reinel Osorio <rosorior@uqvirtual.edu.co>
2026-09-26 00:23:33 -0500

    Se borra comentario

--- src/main/java/factory/CursoFactory.java
+++ src/main/java/factory/CursoFactory.java
@@ -3,6 +3,5 @@ import model.Curso;
 
 public abstract class CursoFactory {
 
-    /** Metodo fabrica: cada subclase decide que tipo concreto de Curso construir. */
     public abstract Curso crearCurso(DatosCurso datos);
 }
